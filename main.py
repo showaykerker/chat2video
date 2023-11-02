ecc520ea6aa8fba57d7903891232f4ffd0c96659
Hsu Hsiu Wei <df405102@gmail.com>
2023-11-03 00:48:54 +0800

    refactor code

--- main.py
+++ main.py
@@ -1,75 +1,117 @@
-from moviepy.editor import ImageClip, concatenate_videoclips, clips_array, AudioFileClip, AudioClip, CompositeVideoClip, concatenate_audioclips
-from moviepy.audio.AudioClip import CompositeAudioClip
-from moviepy.video.fx.resize import resize
-from moviepy.video.fx.all import margin
+import json
 import os
-import json 
-from PIL import Image
-import numpy as np
 import subprocess
 
-# Configure frames
-config = [
-    {"img": ["img1.jpg"], "duration": 2},
-    {"img": ["img1.jpg", "img2.jpg"], "duration": 2}, 
-    {"img": ["img2.jpg", "img3.jpg"], "duration": 4}
-]
+from typing import List, Tuple, Union
 
-audioclip = AudioFileClip("discord-notification.mp3").subclip(0.0, 0.5)
+import numpy as np
+from PIL import Image
+from moviepy.audio.AudioClip import CompositeAudioClip
+from moviepy.editor import AudioClip
+from moviepy.editor import AudioFileClip
+from moviepy.editor import VideoClip
+from moviepy.editor import CompositeVideoClip
+from moviepy.editor import ImageClip
+from moviepy.editor import clips_array
+from moviepy.editor import concatenate_audioclips
+from moviepy.editor import concatenate_videoclips
+from moviepy.video.fx.all import margin
 
-# Get max height of images
-max_height = -1
-max_width = -1
-for frame in config:
-    height = -1
-    for img in frame["img"]:
-        im = Image.open(os.path.join("images", img))
-        height += im.height
-        max_width = max(max_width, im.width)
-    max_height = max(max_height, height)
 
-print(f"max height: {max_height}, max width: {max_width}")
-assert max_height > 0, max_height
-assert max_width > 0, max_width
-max_height *= 1.05
-
-# Build video clips
-clips = []
-timestamps = [0.0]
-
-for i, frame in enumerate(config):
-    if i != len(config)-1:
-        timestamps.append(timestamps[-1] + frame["duration"] - 0.1)
-    img_files = [os.path.join("images", f) for f in frame["img"]]
-    
-    if len(img_files) == 1:
-        # Single image => center 
-        clip = ImageClip(img_files[0]).set_position('center').set_duration(frame['duration'])
+def get_config() -> List[dict]:
+    if os.path.isfile("config.json"):
+        use_config = "config.json"
     else:
-        # Multiple images => concatenate vertically
-        imgs = [ImageClip(f).set_position('center').set_duration(frame['duration']) for f in img_files]
-        clip = clips_array([[im] for im in imgs])
+        assert os.path.isfile("example.config.json"), ""\
+            "Must include either config.json or "\
+            "example.config.json in the directory."
+        use_config = "example.config.json"
 
-    w, h = clip.size
-    padding = int((max_height-h)/2.)    
-    clip = margin(clip, mar=None, left=0, right=0, top=padding, bottom=padding, color=(0, 0, 0), opacity=1.0)
-    clips.append(clip)
+    with open(use_config, "r") as f:
+        config = json.load(f)
+    return config
 
-# Concatenate clips     
-final = concatenate_videoclips(clips)
 
-def make_frame(t):
-    numpy_array = np.array([0, 0])
-    return numpy_array
- 
-# creating audio clip
-empty_audio = AudioClip(make_frame, duration = final.duration)
+def get_audio_clip() -> AudioFileClip:
+    assert os.path.isfile("discord-notification.mp3"), ""\
+        "Must have discord-notification.mp3 in the directory."
+    return AudioFileClip("discord-notification.mp3").subclip(0.0, 0.5)
 
-mixed = CompositeAudioClip([empty_audio] + [audioclip.set_start(t).set_duration(0.5) for t in timestamps])
 
-final.audio = mixed
+def get_max_img_height(config: List[str]) -> float:
+    max_height = -1
+    for frame in config:
+        height = sum([Image.open(os.path.join("images", img)).height
+                      for img in frame["img"]])
+        max_height = max(max_height, height)
 
-final.write_videofile('output.mp4', fps=6, audio_codec='aac')
+    print(f"max height: {max_height}")
+    assert max_height > 0, max_height
+
+    return max_height * 1.05
+
+
+def build_video_clips(
+        config: List[dict],
+        max_height: float) -> Tuple[VideoClip, List[float]]:
+    clips = []
+    timestamps = [0.0]
+
+    for i, frame in enumerate(config):
+        if i != len(config)-1:
+            timestamps.append(timestamps[-1] + frame["duration"] - 0.1)
+
+        img_files = [os.path.join("images", f) for f in frame["img"]]
+
+        if len(img_files) == 1:
+            # Single image => center
+            clip = ImageClip(img_files[0]).set_position(
+                'center').set_duration(frame['duration'])
+        else:
+            # Multiple images => concatenate vertically
+            imgs = [ImageClip(f).set_position('center').set_duration(
+                frame['duration']) for f in img_files]
+            clip = clips_array([[im] for im in imgs])
+
+        w, h = clip.size
+        padding = int((max_height-h) / 2.)
+        clip = margin(clip, top=padding, bottom=padding)
+        clips.append(clip)
+
+    # Concatenate clips
+    final = concatenate_videoclips(clips)
+
+    return final, timestamps
+
+
+def get_empty_audio_clip(duration: float) -> AudioClip:
+    def _make_frame(t):
+        numpy_array = np.array([0, 0])
+        return numpy_array
+    return AudioClip(_make_frame, duration=duration)
+
+
+def mix_audio(
+        empty_audio: Union[AudioClip, AudioFileClip],
+        sound: Union[AudioClip, AudioFileClip],
+        timestamps: List[float]) -> AudioClip:
+    return CompositeAudioClip(
+        [empty_audio] +
+        [sound.set_start(t).set_duration(0.5) for t in timestamps])
+
+if __name__ == '__main__':
+
+    config = get_config()
+    discord_notification_clip = get_audio_clip()
+
+    max_height = get_max_img_height(config)
+    video_clip, timestamps = build_video_clips(config, max_height)
+
+    empty_audio = get_empty_audio_clip(duration=video_clip.duration)
+    mixed = mix_audio(empty_audio, discord_notification_clip, timestamps)
+
+    video_clip.audio = mixed
+    video_clip.write_videofile('output.mp4', fps=6, audio_codec='aac')
 
 
 
